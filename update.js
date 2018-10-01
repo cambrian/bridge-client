f816835b069590a64ecaa0daa6404f62f0d1b8ee
Sanjay Kannan <whaatt@gmail.com>
2018-10-01 00:48:04 -0400

    less broken

--- update.js
+++ update.js
@@ -1,3 +1,6 @@
-const props = Object.getOwnPropertyNames(require('./src/schemas.json').definitions)
-const quoted = props.map(prop => '\'' + prop + '\'')
+#!/usr/bin/env node
+if (process.argv.length < 3) process.exit(1)
+const props = Object.getOwnPropertyNames(require(process.argv[2]).definitions)
+const propsAll = props.concat(['string', 'number', 'boolean', 'symbol']) // Add primitive types.
+const quoted = propsAll.map(prop => '\'' + prop + '\'')
 console.log('export type SchemaRefs = ' + quoted.join('\n  | '))
--- update.sh
+++ update.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-./node_modules/.bin/typescript-json-schema schema.tsconfig.json "*" > src/schemas.json
-node update.js > src/raw.ts
-echo 'export const Schemas = {' >> src/raw.ts
-sed '1d;$d' src/schemas.json >> src/raw.ts
-sed 'h;s/[^ ].*//;s/    /  /g;G;s/\n *//' src/raw.ts > src/double.ts
-sed "s/\"/'/g" src/double.ts > src/schemas.ts
-rm src/schemas.json src/raw.ts
+TEMP_JSON=/tmp/schemas.json
+# Generates JSON schema from generated types. Does post-processing to make things clean.
+./node_modules/.bin/typescript-json-schema schema.tsconfig.json "*" > $TEMP_JSON
+PREPEND_TYPES=$(node update.js $TEMP_JSON)
+PREPEND_EXPORT=$(echo 'export const Schemas = {')
+NO_TRAILING=$(sed '1d;$d' $TEMP_JSON)
+VALID_TABS=$(echo "$NO_TRAILING" | sed 'h;s/[^ ].*//;s/    /  /g;G;s/\n *//')
+NO_DOUBLE=$(echo "$VALID_TABS" | sed "s/\"/'/g")
+echo "$PREPEND_TYPES" > src/schemas.ts
+echo "$PREPEND_EXPORT" >> src/schemas.ts
+echo "$NO_DOUBLE" >> src/schemas.ts
 echo "JSON schema generated for the latest types."
 echo "You may update the Version type now."
